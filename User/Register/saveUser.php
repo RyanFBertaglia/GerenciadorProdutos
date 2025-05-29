a67ca2b2bcf9311796152bb9b91dff3683bee219
Ryan <ryanfernandesbertaglia@gmail.com>
2025-05-29 08:22:50 -0300

    Cadastro user

--- User/Register/saveUser.php
+++ User/Register/saveUser.php
@@ -3,21 +3,24 @@
 $host = "localhost";
 $usuario = "root";
 $senha = "";
-$banco = "cadastro";
+$banco = "mancha";
+
 
 $conexao = new mysqli($host, $usuario, $senha, $banco);
+
 
 if ($conexao->connect_error) {
     die("Erro de conexão: " . $conexao->connect_error);
 }
 
+
 function validarCPF($cpf) {
     $cpf = preg_replace('/[^0-9]/', '', $cpf);
-    
+   
     if (strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)) {
         return false;
     }
-    
+   
     for ($t = 9; $t < 11; $t++) {
         for ($d = 0, $c = 0; $c < $t; $c++) {
             $d += $cpf[$c] * (($t + 1) - $c);
@@ -30,6 +33,7 @@ function validarCPF($cpf) {
     return true;
 }
 
+
 $nome = htmlspecialchars($_POST['nome']);
 $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
@@ -38,9 +42,11 @@ $phone = preg_replace('/[^0-9]/', '', $_POST['phone']);
 $address = htmlspecialchars($_POST['address']);
 $birthdate = $_POST['birthdate'];
 
+
 if (!validarCPF($cpf)) {
     die("CPF inválido!");
 }
+
 
 $sql_verifica = "SELECT id FROM usuarios WHERE email = ?";
 $stmt_verifica = $conexao->prepare($sql_verifica);
@@ -48,19 +54,24 @@ $stmt_verifica->bind_param("s", $email);
 $stmt_verifica->execute();
 $stmt_verifica->store_result();
 
+
 if ($stmt_verifica->num_rows > 0) {
     die("Este e-mail já está cadastrado!");
 }
 
-$sql_insere = "INSERT INTO usuarios (nome, email, senha, cpf, telefone, endereco, data_nascimento) 
+
+$sql_insere = "INSERT INTO usuarios (nome, email, senha, cpf, telefone, endereco, data_nascimento)
                VALUES (?, ?, ?, ?, ?, ?, ?)";
 $stmt_insere = $conexao->prepare($sql_insere);
+
 
 if ($stmt_insere === false) {
     die("Erro na preparação da query: " . $conexao->error);
 }
 
+
 $stmt_insere->bind_param("sssssss", $nome, $email, $senha, $cpf, $phone, $address, $birthdate);
+
 
 if ($stmt_insere->execute()) {
     echo "<h1>Cadastro realizado com sucesso!</h1>";
@@ -68,6 +79,7 @@ if ($stmt_insere->execute()) {
 } else {
     echo "Erro ao cadastrar: " . $stmt_insere->error;
 }
+
 
 // Fechar conexões
 $stmt_verifica->close();
--- User/registerUser.php
+++ User/registerUser.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Cadastro Completo</title>
-  
+ 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-  
+ 
     <link rel="stylesheet" href="../static/style/register.css">
 </head>
 <body>
+
 
 <aside>
         <div class="menu-icon">
@@ -39,22 +40,26 @@
     <main>
     <img src="../static/img/predio.png" alt="Prédio" class="building-img" />
 
+
     <h1>Cadastro Completo</h1>
-    <form action="./RegistersalvarUser.php" method="POST" onsubmit="return validarFormulario()">
+    <form action="./Register/saveUser.php" method="POST" onsubmit="return validarFormulario()">
         <div class="form-group">
             <label for="nome">Nome Completo:</label>
             <input type="text" name="nome" id="nome" required>
         </div>
+
 
         <div class="form-group">
             <label for="email">E-mail:</label>
             <input type="email" name="email" id="email" required>
         </div>
 
+
         <div class="form-group">
             <label for="senha">Senha:</label>
             <input type="password" name="senha" id="senha" required minlength="6">
         </div>
+
 
         <div class="form-group">
             <label for="cpf">CPF:</label>
@@ -62,23 +67,28 @@
             <div id="cpf-error" class="error"></div>
         </div>
 
+
         <div class="form-group">
             <label for="phone">Telefone:</label>
             <input type="tel" name="phone" id="phone" required placeholder="(00) 00000-0000">
         </div>
+
 
         <div class="form-group">
             <label for="address">Endereço:</label>
             <input type="text" name="address" id="address" required>
         </div>
 
+
         <div class="form-group">
             <label for="birthdate">Data de Nascimento:</label>
             <input type="date" name="birthdate" id="birthdate" required>
         </div>
 
+
         <button class="button-class" type="submit">Cadastrar</button>
     </form>
+
 
     <script src="../static/js/validation.js"></script>
     </main>
